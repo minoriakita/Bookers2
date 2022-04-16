7237a3707850cc18dfb8ca73b3b255220b386174
minoriakita <minori.akita@gmail.com>
2022-04-16 15:20:48 +0000

    [Update] 4-25 now best

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get '/homes/about' => 'homes#about', as: 'about'
-  
+
   resources :books, only: [:new, :create,:index, :show]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update]
 
 end
