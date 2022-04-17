6f732fe7dc3d0630d8ed58e2997c483dbd14d227
minoriakita <minori.akita@gmail.com>
2022-04-17 12:37:15 +0000

    [Update] 4-25 新規投稿作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,24 +2,25 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(current_user.id)
+    redirect_to books_path
   end
-  
+
   def index
     @books = Book.all
   end
 
   def show
-    @book= Book.find(params[:id])
+    @book = Book.find(params[:id])
+
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
   end
