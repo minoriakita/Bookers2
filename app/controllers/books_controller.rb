05f732de52882defec9aec85561bd0a24ecc4852
minoriakita <minori.akita@gmail.com>
2022-04-17 06:35:24 +0000

    [Update] 各ページへのリンク作成、新規投稿画面作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,26 @@
 class BooksController < ApplicationController
   def new
-    @books = Book.new
+    @book = Book.new
   end
-
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(current_user.id)
+  end
+  
   def index
+    @books = Book.all
   end
 
   def show
+    @book= Book.find(params[:id])
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
-
