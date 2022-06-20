9d5d2b1e3482fd3b7c0368cc1eba81ca993e09fc
minoriakita <minori.akita@gmail.com>
2022-06-20 12:21:04 +0000

    [Finish]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.active_job.queue_adapter = :inline
 
-  config.hosts << "ecbe7269fafa4258aa8f0ff5573de4e7.vfs.cloud9.ap-northeast-1.amazonaws.com"
+config.hosts << "ecbe7269fafa4258aa8f0ff5573de4e7.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
